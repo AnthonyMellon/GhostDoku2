b01045571f181da71155928b799a5aed48d41e16
Anthony Mellon (1000066356) <MELLAM1@student.op.ac.nz>
2022-07-18 17:02:06 +1200

    added UI canvas and Pause menu canvas to game scene. Pause menu canvas can be toggled with a button on the UI canvas

---
+++ GhostDoku2/Assets/Scripts/GameUI.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameUI : MonoBehaviour
+{
+    public GameObject pauseMenu;
+    
+    public void togglePause()
+    {
+        pauseMenu.SetActive(!pauseMenu.activeSelf);
+    }
+}

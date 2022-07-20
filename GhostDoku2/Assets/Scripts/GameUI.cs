bf5f4eb0cd3470ca5462b4ab87afca25591f4d7f
Anthony <anthonymellon@yahoo.com>
2022-07-20 21:44:27 +1200

    pause button no longer toggles pause, it only pauses the game. added unpause button to pause menu. added return to main menu button to pause menu

--- GhostDoku2/Assets/Scripts/GameUI.cs
+++ GhostDoku2/Assets/Scripts/GameUI.cs
@@ -5,9 +5,4 @@ using UnityEngine;
 public class GameUI : MonoBehaviour
 {
     public GameObject pauseMenu;
-    
-    public void togglePause()
-    {
-        pauseMenu.SetActive(!pauseMenu.activeSelf);
-    }
 }

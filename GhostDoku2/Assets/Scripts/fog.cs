d79a85b250d755242c8ead21fa01e51c758a438e
Anthony <anthonymellon@yahoo.com>
2022-07-20 22:38:12 +1200

    added fog tiles that spawn at the start of the game, they can be clicked to be removed

---
+++ GhostDoku2/Assets/Scripts/fog.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fog : MonoBehaviour
+{
+    private void OnMouseOver()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ GhostDoku2/Assets/Scripts/fogManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fogManager : MonoBehaviour
+{
+    public GameObject fogTile;
+
+    public float tileWidth;
+    public float tileHeight;
+
+    //Eventually moved to map manager script
+    public float mapWidth;
+    public float mapHeight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnFog();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void spawnFog()
+    {
+        float nTilesX = mapWidth / tileWidth;
+        float nTilesY = mapHeight / tileHeight;
+
+        for(int x = 0; x < nTilesX; x++)
+        {
+            for(int y = 0; y < nTilesY; y++)
+            {
+                float xPos = x - (nTilesX / 2);
+                xPos *= tileWidth;
+
+                float yPos = y - (nTilesY / 2);
+                yPos *= tileHeight;
+
+                GameObject currentTile = Instantiate(fogTile, new Vector3(xPos, yPos, 0), new Quaternion(), transform);
+                currentTile.transform.localScale = new Vector3(tileWidth, tileHeight, 0);
+            }
+        }
+    }
+}
